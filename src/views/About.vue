dee9b66e68ba4bc8e4d1256a8c18c90e83f6588e
Johannes <johannes.idarsson@gmail.com>
2020-10-13 11:16:55 +0200

    important update

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <b>This is an updated version, built automatically</b>
+    </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <b>This is an updated version, built automatically</b>
+    </div>
 </template>
 
 <script>
@@ -10,9 +11,9 @@
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+    name: "Home",
+    components: {
+        HelloWorld,
+    },
 };
 </script>
